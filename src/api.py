f01d4df6516960c3b4cbd5195e2fd988366abe09
FearDawn <firdan.booo@gmail.com>
2022-09-25 15:21:33 +0700

    testing

---
+++ src/api.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "<h1 style='color:blue'>Hello PMDS!</h1>"
+
+def main():
+    app.run(host = '0.0.0.0', port = 8080)
+
+if __name__ == "__main__":
+    main()
